182e9ee31ca7e9212f2ca0a57e74812db37a9db9
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-02-17 02:56:36 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,6 +6,12 @@ class Show < ActiveRecord::Base
   
   def self.most_popular_show
    self.where("rating = ?", self.highest_rating).first
+  end 
+  
+  def self.lowest_rating 
+    self.minimum(:rating)
+  end 
+  
   
   
 end 
